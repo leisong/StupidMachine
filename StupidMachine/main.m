fcc88e3e5071903194e2c914f6999d19caf7cb9a
zhangjiji <programmer.zhangjiji@gmail.com>
2012-11-23 22:12:01 +0800

    Initial Commit

---
+++ StupidMachine/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StupidMachine
+//
+//  Created by Zhang Zhe on 12-11-23.
+//  Copyright Avalon Game Art 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
